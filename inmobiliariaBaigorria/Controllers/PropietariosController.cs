437265715adfc5c26c1cc7408741f8f075f7c17f
PatoBaigorria <87587522+PatoBaigorria@users.noreply.github.com>
2023-08-21 16:31:17 -0300

    Vistas Creadas Propietario
    Vistas Propietario

--- inmobiliariaBaigorria/Controllers/PropietariosController.cs
+++ inmobiliariaBaigorria/Controllers/PropietariosController.cs
@@ -1,64 +1,96 @@
-﻿using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using inmobiliariaBaigorria.Models;
 
-namespace inmobiliariaBaigorria.Controllers;
-
-public class PropietariosController : Controller
+namespace inmobiliariaBaigorria.Controllers
 {
-    private readonly ILogger<PropietariosController> _logger;
-
-    public PropietariosController(ILogger<PropietariosController> logger)
+    public class PropietariosController : Controller
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        RepositorioPropietario repositorio = new RepositorioPropietario();
-        List<Propietario> propietarios = repositorio.ObtenerPropietarios();
-        return View(propietarios);
-    }
-    [HttpGet]
-    public IActionResult Create()
-    {
-        return View();
-    }
-    [HttpPost]
-    public IActionResult Create(Propietario propietario)
-    {
-        try
+        // GET: Propietarios
+        public ActionResult Index()
         {
             RepositorioPropietario repositorio = new RepositorioPropietario();
-            repositorio.Alta(propietario);
-            return RedirectToAction("Index");
-        }
-        catch (System.Exception)
-        {
-            throw;
-        }
-    }
 
-    [HttpGet]
-    public IActionResult Update(int id)
-    {
-        RepositorioPropietario repositorio = new RepositorioPropietario();
-        Propietario x = repositorio.ObtenerPorId(id);
-        return View(x);
-    }
-    [HttpPost]
-    public IActionResult Update(Propietario propietario)
-    {
-        try
-        {
-            RepositorioPropietario repositorio = new RepositorioPropietario();
-            repositorio.Alta(propietario);
-            return RedirectToAction("Index");
-        }
-        catch (System.Exception)
-        {
-            throw;
+            return View(repositorio.ObtenerPropietarios());
         }
 
+        // GET: Propietarios/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Propietarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Propietarios/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Propietarios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Propietarios/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Propietarios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Propietarios/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
